a5440bdf2c618cbfddaf57d6fbdf8831e642a2a9
Imran Sobo <imranjedantri@gmail.com>
2023-11-27 21:12:17 +0100

    cleanup

--- main.cpp
+++ main.cpp
@@ -6,18 +6,14 @@ using namespace std;
 
 int main() {
 
-  /*list<int> l{1,2,3,5};
-  l.resize(10);
-  //for(list<int>::iterator it = l.begin(); it != l.end(); it++)
-    //(*it) = 7;
-  for(auto it = l.rbegin(); it != l.rend(); it++)
-    cout<<*it<<" ";*/
   List<int> l1(3,2);
   List<int> l2(5,3);
 
   cout<<endl;
+
   for(List<int>::Iterator it = l1.Begin(); it != l1.End(); it++)
     (*it)=7;
+
   for(auto it = l2.Begin(); it != l2.End(); it++)
     cout<<*it<<" ";
 }
